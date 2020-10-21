71a52bc15b7f3fd399018b98bbc6102f88e65d75
JuliaFray <julia.pticami@gmail.com>
2020-10-21 16:56:06 +0700

    Updates

---
+++ precache-manifest.e96419539f0aa7cdd3f5f20f08573912.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1a323fe0a2e3968d3eb1fc518b112c7",
+    "url": "/JuliaFray/Academy-Task-3/index.html"
+  },
+  {
+    "revision": "192cea7315c0190ccc9d",
+    "url": "/JuliaFray/Academy-Task-3/static/css/main.8c7fc1cb.chunk.css"
+  },
+  {
+    "revision": "043a53b68a06c96f308e",
+    "url": "/JuliaFray/Academy-Task-3/static/js/2.64ae55fc.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/JuliaFray/Academy-Task-3/static/js/2.64ae55fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "192cea7315c0190ccc9d",
+    "url": "/JuliaFray/Academy-Task-3/static/js/main.37fe5d1e.chunk.js"
+  },
+  {
+    "revision": "bf7c827056e40dfac876",
+    "url": "/JuliaFray/Academy-Task-3/static/js/runtime-main.e2be3ce7.js"
+  }
+]);
