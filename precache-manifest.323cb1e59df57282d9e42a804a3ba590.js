9fe8b4644f41fb823d0378e159337da114faa3ea
JuliaFray <julia.pticami@gmail.com>
2020-10-21 17:06:14 +0700

    Updates

--- precache-manifest.323cb1e59df57282d9e42a804a3ba590.js
+++ precache-manifest.323cb1e59df57282d9e42a804a3ba590.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "391cae2c39eb215e63bf5d3c98225765",
+    "revision": "8e38a34b79da681179a5d368fcfe00ee",
     "url": "/Academy-Task-3/index.html"
   },
   {
-    "revision": "c695a46075c60432c8ba",
+    "revision": "b8160a9b4e00e3c08782",
     "url": "/Academy-Task-3/static/css/main.8c7fc1cb.chunk.css"
   },
   {
-    "revision": "ac2bb8722c8929ff6854",
-    "url": "/Academy-Task-3/static/js/2.37a9ad99.chunk.js"
+    "revision": "bc91e24b2c2dfba2231f",
+    "url": "/Academy-Task-3/static/js/2.c673b9de.chunk.js"
   },
   {
     "revision": "c87550333da8df8ab4c80bd1d6e9defb",
-    "url": "/Academy-Task-3/static/js/2.37a9ad99.chunk.js.LICENSE.txt"
+    "url": "/Academy-Task-3/static/js/2.c673b9de.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c695a46075c60432c8ba",
-    "url": "/Academy-Task-3/static/js/main.1e4edb95.chunk.js"
+    "revision": "b8160a9b4e00e3c08782",
+    "url": "/Academy-Task-3/static/js/main.6e709896.chunk.js"
   },
   {
     "revision": "a6aba5dbe3bd5cc9933f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Academy-Task-3/precache-manifest.454a8191d32847901d4c2a21d3fc230d.js"
+  "/Academy-Task-3/precache-manifest.323cb1e59df57282d9e42a804a3ba590.js"
 );
 
 self.addEventListener('message', (event) => {
